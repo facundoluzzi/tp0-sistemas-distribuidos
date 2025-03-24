600a9f4b9fb8e45190817adacb08e6fea1b807e2
fluzzi_meli <facundo.luzzi@mercadolibre.com>
2025-03-23 22:25:09 -0300

    add validate echo server

---
+++ validar-echo-server.sh
@@ -0,0 +1,13 @@
+SERVER_NAME="server"
+SERVER_PORT=12345
+HELLO_WORLD_MESSAGE="hello_world"
+DOCKER_NETWORK="tp0_testing_net"
+
+SERVER_RESPONSE=$(docker run --rm --network="$DOCKER_NETWORK" busybox:latest sh -c "echo '$TEST_MESSAGE' | nc $SERVER_NAME $SERVER_PORT")
+
+echo $SERVER_RESPONSE
+if [ "$SERVER_RESPONSE" == "$HELLO_WORLD_MESSAGE" ]; then
+    echo "action: test_echo_server | result: success"
+else
+    echo "action: test_echo_server | result: fail"
+fi
