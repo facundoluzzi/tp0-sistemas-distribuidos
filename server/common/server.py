dfc3f47ea800a582bc8368a7aef9d3253c7e6fff
fluzzi <fnluzzi@gmail.com>
2025-03-27 20:28:09 -0300

    add exercise 8

--- server/common/server.py
+++ server/common/server.py
@@ -5,6 +5,7 @@ import logging
 import signal
 import struct
 import sys
+import threading
 import traceback
 
 from common.utils import Bet, has_won, store_bets, load_bets
@@ -20,6 +21,8 @@ class Server:
         self.clients_finished = set()
         self.is_running = True
         
+        self.lock = threading.Lock()
+
         self.max_connections = int(os.environ.get("MAX_CONNECTIONS", 5))
         
         signal.signal(signal.SIGTERM, self.graceful_shutdown)
@@ -37,7 +40,7 @@ class Server:
             client_sock = self.__accept_new_connection()
             
             if len(self.client_connections) >= self.max_connections:
-                logging.info(f'max clients connected {self.max_connections}, rejecting new connection')
+                # logging.info(f'max clients connected {self.max_connections}, rejecting new connection')
                 client_sock.sendall(b"ERROR: Maximum number of agencies reached\n")
                 client_sock.close()
                 continue 
@@ -45,6 +48,22 @@ class Server:
             self.client_connections.append(client_sock)
             self.__handle_client_connection(client_sock)
             
+            try:
+                client_sock = self.__accept_new_connection()
+                
+                with self.lock:
+                    if len(self.client_connections) >= self.max_connections:
+                        # logging.info(f'max clients connected {self.max_connections}, rejecting new connection')
+                        client_sock.sendall(b"ERROR: Maximum number of agencies reached\n")
+                        client_sock.close()
+                        continue 
+                    
+                    self.client_connections.append(client_sock)
+                
+                client_thread = threading.Thread(target=self.__handle_client_connection, args=(client_sock,), daemon=True)
+                client_thread.start()
+            except OSError:
+                break  
     def __handle_client_connection(self, client_sock):
         """
         Read message from a specific client socket and closes the socket
@@ -72,41 +91,42 @@ class Server:
                     for line in lines[1:]:
                         parts = line.split("|")
                         if len(parts) != 6:
-                            logging.warning(f"action: parse_bet | result: fail | reason: invalid_format | data: {line}")
+                            # logging.warning(f"action: parse_bet | result: fail | reason: invalid_format | data: {line}")
                             continue  
 
                         bet = Bet(*parts)
                         bets.append(bet)
 
-                    logging.info(f"action: parse_bets | result: success | count: {len(bets)}")
+                    # logging.info(f"action: parse_bets | result: success | count: {len(bets)}")
 
                     store_bets(bets)
 
-                    logging.info(f'action: apuesta_recibida | result: success | cantidad: ${len(bets)}')
+                    # logging.info(f'action: apuesta_recibida | result: success | cantidad: ${len(bets)}')
 
                     ack_response = utils.ACK_MESSAGE.format("-".join(str(bet.number) for bet in bets))
                     client_sock.sendall("{}\n".format(ack_response).encode('utf-8'))
                 elif msg_type == "delivery-ended":
-                    self.clients_finished.add(lines[1])
-                    logging.info(f'Cantidad de clientes que terminaron: {len(self.clients_finished)}')
-                    if len(self.client_connections) == utils.MAX_CONNECTIONS:
-                        logging.info("action: sorteo | result: success")
+                    with self.lock:
+                        self.clients_finished.add(lines[1])
+                        if len(self.clients_finished) == self.max_connections:
+                            logging.info("action: sorteo | result: success")
                     continue
                 elif msg_type == "ask-winners":
-                    if len(self.clients_finished) == utils.MAX_CONNECTIONS:
-                        bets = load_bets()
-                        winners = [bet for bet in bets if has_won(bet)]
-                        filtered_winners = [bet for bet in winners if int(bet.agency) == int(lines[1])]
-                        count_winners = len(filtered_winners)
-                        client_sock.sendall("{}\n".format(count_winners).encode('utf-8'))
-                    else:
-                        client_sock.sendall("{}\n".format(utils.PENDING_RAFFLE_MESSAGE).encode('utf-8'))
-                        
+                    with self.lock:
+                        if len(self.clients_finished) == self.max_connections:
+                            bets = load_bets()
+                            winners = [bet for bet in bets if has_won(bet)]
+                            filtered_winners = [bet for bet in winners if int(bet.agency) == int(lines[1])]
+                            count_winners = len(filtered_winners)
+                            client_sock.sendall(f"{count_winners}\n".encode('utf-8'))
+                        else:
+                            client_sock.sendall(f"{utils.PENDING_RAFFLE_MESSAGE}\n".encode('utf-8'))
                     break
         except OSError as e:
             logging.error("action: receive_message | result: fail | error: {e}")
         finally:
-            self.client_connections.remove(client_sock)
+            with self.lock:
+                self.client_connections.remove(client_sock)
             client_sock.close()
             
     def __accept_new_connection(self):
@@ -129,10 +149,11 @@ class Server:
         self._server_socket.close()
         logging.info("server socket was closed")
         
-        logging.info(f"closing {len(self.client_connections)} client connections")
-
-        for conn in self.client_connections:
-            conn.close()
+        with self.lock:
+            logging.info(f"closing {len(self.client_connections)} client connections")
+            for conn in self.client_connections:
+                conn.close()
+            self.client_connections.clear()
             
         logging.info("client connections were closed successfully")
         
