c696f7995baad18243d93f079e9d3e03e85c5f4e
fluzzi <fnluzzi@gmail.com>
2025-03-30 15:51:48 -0300

    update script

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -3,7 +3,7 @@ SERVER_PORT=12345
 HELLO_WORLD_MESSAGE="hello_world"
 DOCKER_NETWORK="tp0_testing_net"
 
-SERVER_RESPONSE=$(docker run --rm --network="$DOCKER_NETWORK" busybox:latest sh -c "echo '$TEST_MESSAGE' | nc $SERVER_NAME $SERVER_PORT")
+SERVER_RESPONSE=$(docker run --rm --network="$DOCKER_NETWORK" busybox:latest sh -c "echo '$HELLO_WORLD_MESSAGE' | nc $SERVER_NAME $SERVER_PORT")
 
 echo $SERVER_RESPONSE
 if [ "$SERVER_RESPONSE" == "$HELLO_WORLD_MESSAGE" ]; then
